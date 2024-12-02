65e30d38fa3e56b1695d1cff0194945fa788dfc3
Gregory M Kohler <gregorykohler@gmail.com>
2024-12-01 18:16:53 -0800

    day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -24,9 +25,26 @@ fn main() {
 
     let total_distance: u32 = vec1
         .iter()
-        .zip(vec2)
+        .zip(vec2.clone())
         .map(|(one, two)| one.abs_diff(two))
         .sum();
 
+    let sim_score = similarity_score(vec1, vec2);
+
     println!("Total difference: {total_distance}");
+    println!("Similarity score: {sim_score}");
+}
+
+fn similarity_score(vec1: Vec<u32>, vec2: Vec<u32>) -> u32 {
+    let unique_in_one: HashSet<u32> = HashSet::from_iter(vec1.iter().cloned());
+    let occurrences_in_two: HashMap<u32, u32> =
+        vec2.iter().fold(HashMap::new(), |mut acc, c| {
+            *acc.entry(*c).or_insert(0) += 1;
+            acc
+        });
+
+    unique_in_one
+        .iter()
+        .map(|num| *num * occurrences_in_two.get(num).copied().unwrap_or(0))
+        .sum()
 }
